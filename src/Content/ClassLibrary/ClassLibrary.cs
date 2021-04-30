04dbf100d5a5d06fb109cf9ec29028bfd86f58e5
John Nyingi <JNyingi@resolution.lan>
2021-04-30 21:29:41 +0300

    Added Class Library Temp

---
+++ src/Content/ClassLibrary/ClassLibrary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class ClassLibrary: IClassLibrary
+    {
+        public ClassLibrary(/*add properties to inject*/)
+        {
+
+        }
+    }
+}
---
+++ src/Content/ClassLibrary/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public static class ServiceCollectionExtensions
+    {
+        
+        public static void AddClassLibrary(this IServiceCollection services)
+        {
+            services.AddSingleton<IClassLibrary, ClassLibrary>(v => new ClassLibrary(/*Add properties to inject*/));
+        }
+    }
+}
